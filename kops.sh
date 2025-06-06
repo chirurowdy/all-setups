4741239825098ae7e296037c9e372b09247f6c93
chiranjeevi keerthi <chiranjeevikeerthi77777@gmail.com>
2025-06-06 17:02:01 +0530

    Update kops.sh

--- kops.sh
+++ kops.sh
@@ -6,13 +6,13 @@
 #! /bin/bash
 aws configure
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-wget https://github.com/kubernetes/kops/releases/download/v1.31.0/kops-linux-amd64
+wget https://github.com/kubernetes/kops/releases/download/v1.30.3/kops-linux-amd64
 chmod +x kops-linux-amd64 kubectl
 mv kubectl /usr/local/bin/kubectl
 mv kops-linux-amd64 /usr/local/bin/kops
 
-aws s3api create-bucket --bucket chirulocalk8sbucket.k8s.local --region us-east-1
-aws s3api put-bucket-versioning --bucket chirulocalk8sbucket.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
-export KOPS_STATE_STORE=s3://chirulocalk8sbucket.k8s.local
-kops create cluster --name chiru.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
-kops update cluster --name chiru.k8s.local --yes --admin
+aws s3api create-bucket --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1
+aws s3api put-bucket-versioning --bucket cloudanddevopsbyraham0073456.k8s.local --region us-east-1 --versioning-configuration Status=Enabled
+export KOPS_STATE_STORE=s3://cloudanddevopsbyraham0073456.k8s.local
+kops create cluster --name rahams.k8s.local --zones us-east-1a --master-count=1 --master-size t2.medium --node-count=2 --node-size t2.micro
+kops update cluster --name rahams.k8s.local --yes --admin
